42fd81431c258c07b06c42ac7db87d98e6a753a8
brunoguez <brunomguez@gmail.com>
2024-08-28 00:51:26 -0300

    ex6

--- ListaExercicios1/App.js
+++ ListaExercicios1/App.js
@@ -7,30 +7,37 @@ import Ex5 from './components/Ex5';
 import Ex6 from './components/Ex6';
 
 export default function App() {
+
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.scrollView}>
-                <Ex1 viewStyle={{display: 'none'}}/>
-                <Ex2 viewStyle={{display: 'none'}}/>
-                <Ex3 viewStyle={{display: 'none'}}/>
-                <Ex4 viewStyle={{display: 'none'}}/>
-                <Ex5 viewStyle={{display: 'auto'}}/>
-                <Ex6 viewStyle={{display: 'auto'}}/>
-                <Text style={styles.text}><b>Exercício 6:&nbsp;</b>Teste</Text>
+                <Ex1 viewStyle={{ display: 'none' }} />
+                <Ex2 viewStyle={{ display: 'none' }} />
+                <Ex3 viewStyle={{ display: 'none' }} />
+                <Ex4 viewStyle={{ display: 'none' }} />
+                <Ex5 viewStyle={{ display: 'none' }} />
+                <Ex6 viewStyle={{ display: 'none' }} />
                 <Text style={styles.text}><b>Exercício 7:&nbsp;</b>Teste</Text>
                 <Text style={styles.text}><b>Exercício 8:&nbsp;</b>Teste</Text>
+                <Text style={styles.text}><b>Exercício 9:&nbsp;</b>Teste</Text>
             </ScrollView>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+    },
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        
+
     },
     scrollView: {
         backgroundColor: 'pink',
--- ListaExercicios1/components/Ex6.js
+++ ListaExercicios1/components/Ex6.js
@@ -1,5 +1,5 @@
-import { StyleSheet, SafeAreaView } from 'react-native'
-import { RadioButton, Divider, Text, Checkbox } from 'react-native-paper';
+import { StyleSheet, SafeAreaView, View, Image, ScrollView } from 'react-native'
+import { RadioButton, Divider, Text, Checkbox, Button, FAB, } from 'react-native-paper';
 import * as React from 'react'
 import { exercises } from '../db';
 import estilos from '../templates';
@@ -91,50 +91,143 @@ const dict = {
     level: mapear('level'),
     mechanic: mapear('mechanic'),
 }
+console.log({ ex, dict })
 
 const Ex6 = ({ viewStyle }) => {
-    const [level, setLevel] = React.useState('iniciante');
-    const [categorias, setCategorias] = React.useState();
 
-    const handleCategoria = (...a) => {
-        setCategorias(new Map())
+    const [level, setLevel] = React.useState('iniciante');
+    const [categorias, setCategorias] = React.useState(
+        Object.fromEntries(Array.from(dict.category)
+            .map(a => [a, false]))
+    );
+    const [equipamentos, setEquipamentos] = React.useState(
+        Object.fromEntries(Array.from(dict.equipment)
+            .map(a => [a, false]))
+    );
+    const [show, setShow] = React.useState(false);
+    const [treinos, setTreinos] = React.useState([]);
+    const [textButton, setTextButton] = React.useState('Mostrar Treinos')
+
+    const handleCategoria = (cat) => {
+        const atual = categorias[cat];
+        setCategorias(({ ...categorias, [cat]: !atual }))
+    }
+    const handleEquipameto = (eq) => {
+        const atual = equipamentos[eq];
+        setEquipamentos(({ ...equipamentos, [eq]: !atual }))
+    }
+    const handleFiltrar = () => {
+        const formatCategorias = Object.entries(categorias)
+            .filter(a => a[1])
+            .map(a => a[0]);
+        const formatEquipamentos = Object.entries(equipamentos)
+            .filter(a => a[1])
+            .map(a => a[0]);
+
+        const filteredExercises = ex.filter(a => a.level == level &&
+            (formatCategorias.length > 0 ? formatCategorias.includes(a.category) : true) &&
+            (formatEquipamentos.length > 0 ? formatEquipamentos.includes(a.equipment) : true))
+
+        setTreinos(filteredExercises);
+
+        setShow((atual) => !atual)
+        setTextButton((atual) => atual == 'Mostrar Treinos' ? 'Voltar' : 'Mostrar Treinos')
     }
 
-    React.useState(() => {
-        console.log(dict)
-    }, []);
-
     return (
+
         <SafeAreaView style={viewStyle}>
             <Text style={styles.text}><b>Exercício 6: </b>Aplicativo de Rastreamento de Exercício</Text>
-            <Divider />
-            <Text style={styles.title} variant="headlineSmall">Level</Text>
-            <RadioButton.Group onValueChange={value => setLevel(value)} value={level}>
-                <RadioButton.Item label="Iniciante" value="iniciante" />
-                <RadioButton.Item label="Intermediário" value="intermediario" />
-                <RadioButton.Item label="Especialista" value="especialista" />
-            </RadioButton.Group>
-            {Array.from(dict.category).map(cat => (
-                <Checkbox
-                    status={categorias.includes(cat) ? "checked" : "unchecked"}
-                    onPress={(a) => handleCategoria(a,cat)}
-                    key={cat}
-                />
-            ))}
-            <Divider />
-        </SafeAreaView>
+            {!show && <View>
+                <Divider />
+                <Text style={styles.title} variant="headlineSmall">Level</Text>
+                <RadioButton.Group onValueChange={value => setLevel(value)} value={level}>
+                    <RadioButton.Item label="Iniciante" value="iniciante" />
+                    <RadioButton.Item label="Intermediário" value="intermediário" />
+                    <RadioButton.Item label="Especialista" value="especialista" />
+                </RadioButton.Group>
+                <Divider />
+                <Text style={styles.title} variant="headlineSmall">Categoria</Text>
+
+                {Array.from(dict.category).map(cat => (
+                    <Checkbox.Item
+                        status={categorias[cat] ? "checked" : "unchecked"}
+                        onPress={() => handleCategoria(cat)}
+                        key={cat}
+                        label={cat.charAt(0).toUpperCase() + cat.slice(1)}
+                    />
+                ))}
+                <Divider />
+                <Text style={styles.title} variant="headlineSmall">Equipamento</Text>
+                {Array.from(dict.equipment).map(eq => (
+                    <Checkbox.Item
+                        status={equipamentos[eq] ? "checked" : "unchecked"}
+                        onPress={() => handleEquipameto(eq)}
+                        key={eq}
+                        label={eq.charAt(0).toUpperCase() + eq.slice(1)}
+                    />
+                ))}
+            </View>}
+            {show && <View>
+                {treinos.map((treino, index) => (
+                    <View key={`${treino.id}_view${index}`} style={styles.card}>
+                        <Image
+                            source={{ uri: `https://github.com/yuhonas/free-exercise-db/blob/main/exercises/${treino.images[0]}?raw=true` }}
+                            style={styles.poster}
+                            key={`${treino.id}_img${index}`}
+                        />
+                        <Text style={styles.title} key={`${treino.id}_txt${index}`}>{treino.name}</Text>
+                    </View>
+                ))}
+            </View>}
+            <Button
+                icon="shoe-sneaker"
+                mode="contained"
+                onPress={handleFiltrar}>
+                <Text style={{ color: 'white' }}>{textButton}</Text>
+            </Button>
+        </SafeAreaView >
     )
 }
 
 const styles = StyleSheet.create({
     ...estilos,
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+    },
     container: {
         flex: 1,
         alignItems: 'center',
     },
     title: {
         textAlign: 'center'
-    }
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 10,
+        marginVertical: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    poster: {
+        width: '100%',
+        height: 150,
+        borderRadius: 10,
+    },
+    titleCard: {
+        marginTop: 10,
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 })
 
 export default Ex6;
