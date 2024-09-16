af88fede6a7e9e9d2009d974b138cb07950e3c96
brunoguez <brunomguez@gmail.com>
2024-09-16 14:08:01 -0300

    splash

--- redeSocial/App.js
+++ redeSocial/App.js
@@ -1,23 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { PaperProvider } from 'react-native-paper';
+import Navigation from './components/Navigation';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+        <Navigation />
     </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// import { StatusBar } from 'expo-status-bar';
+// import { SafeAreaView, Text } from 'react-native';
+// import { PaperProvider } from 'react-native-paper';
+// import { styles } from './src/assets/styles';
+
+// export default function App() {
+
+//   return (
+//     <PaperProvider>
+//       <SafeAreaView style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <StatusBar style="auto" />
+//       </SafeAreaView>
+//     </PaperProvider>
+//   );
+// }
+
---
+++ redeSocial/components/Login.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { SafeAreaView } from 'react-native';
+import { StyleSheet, Text, Safe } from 'react-native-paper';
+
+const Login = () => {
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
