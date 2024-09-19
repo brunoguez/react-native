97250c9300ef4bea938ebda919383f1b5afc995d
brunoguez <brunomguez@gmail.com>
2024-09-18 23:42:15 -0300

    1806

--- redeSocial/App.js
+++ redeSocial/App.js
@@ -1,11 +1,14 @@
 import { useCallback, useEffect, useState } from 'react';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { PaperProvider } from 'react-native-paper';
+import Navigation from './components/Navigation';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native'
 import Login from './components/Login';
-import { StatusBar } from 'react-native';
 
-
+// Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
@@ -14,10 +17,15 @@ export default function App() {
   useEffect(() => {
     async function prepare() {
       try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
         await new Promise(resolve => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
       } finally {
+        // Tell the application to render
         setAppIsReady(true);
       }
     }
@@ -38,11 +46,35 @@ export default function App() {
   return (
     <PaperProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
-          <StatusBar  translucent />
+        <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
           <Login />
         </SafeAreaView>
       </SafeAreaProvider>
     </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { SafeAreaView, Text } from 'react-native';
+// import { PaperProvider } from 'react-native-paper';
+// import { styles } from './src/assets/styles';
+
+// export default function App() {
+
+//   return (
+//     <PaperProvider>
+//       <SafeAreaView style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <StatusBar style="auto" />
+//       </SafeAreaView>
+//     </PaperProvider>
+//   );
+// }
+
--- redeSocial/components/Login.js
+++ redeSocial/components/Login.js
@@ -1,32 +1,71 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Card, Text, TextInput } from 'react-native-paper';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Card, Text, TextInput, Button } from 'react-native-paper';
 
 const Login = () => {
   const [user, setUser] = React.useState("");
 
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#b2ddee',
-      borderWidth: 1
-    }}>
-      <Card style={{ width: '80%', justifyContent: 'center', alignItems: 'center' }}>
-        <Card.Content >
-          <Text>Login</Text>
+    <SafeAreaView style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Text style={styles.title}>Login</Text>
           <TextInput
             label="Email"
             value={user}
             onChangeText={text => setUser(text)}
+            style={[styles.m(10), styles.input]}
           />
+          <TextInput
+            label="Senha"
+            secureTextEntry
+            value={user}
+            right={
+              <TextInput.Icon icon="eye" onPress={(a) => console.log(console.log(JSON.stringify(a, null, 2)))}/>
+            }
+            onChangeText={text => setUser(text)}
+            style={[styles.m(20), styles.input]}
+          />
+          <Button style={styles.m(10)} icon="login" mode="contained" onPress={() => console.log('Pressed')}>
+            Entrar
+          </Button>
+          <Button icon="plus" mode="text" onPress={() => console.log('Pressed')}>
+            Criar conta
+          </Button>
         </Card.Content>
       </Card>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#b2ddee',
+  },
+  card: {
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    flexShrink: 1,
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    width: 300,
+  },
+  m(value) {
+    return {
+      marginBottom: value
+    }
+  }
+});
 
-// const styles = StyleSheet.create({})
+export default Login;
